c8e2d82d5e8b731a27967264997979fbef9ee3e8
mami <mamypoco@gmail.com>
2026-02-03 11:32:44 -0800

    worked SiteDetailSheet

---
+++ Plentifood/Screens/Details/TagChip.swift
@@ -0,0 +1,18 @@
+//
+//  TagChip.swift
+//  Plentifood
+//
+//  Created by Mami on 2/3/26.
+//
+
+import SwiftUI
+
+struct TagChip: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TagChip()
+}
---
+++ Plentifood/Screens/Details/WrapChips.swift
@@ -0,0 +1,18 @@
+//
+//  WrapChips.swift
+//  Plentifood
+//
+//  Created by Mami on 2/3/26.
+//
+
+import SwiftUI
+
+struct WrapChips: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WrapChips()
+}
